1200837236ebe2d4213c74b6baf932b70c384725
yanhick <yannick.dominguez@gmail.com>
2013-05-02 11:18:35 +0200

    added start and complete state for the embedding tutorial

---
+++ embedding_cocktail/start/src/Main.hx
@@ -0,0 +1,16 @@
+/*
+ * Cocktail, HTML rendering engine
+ * http://haxe.org/com/libs/cocktail
+ *
+ * Copyright (c) Silex Labs
+ * Cocktail is available under the MIT license
+ * http://www.silexlabs.org/labs/cocktail-licensing/
+*/
+
+class Main
+{
+	static function main()
+	{
+		new Main();
+	}
+}
